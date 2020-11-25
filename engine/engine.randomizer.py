5c3c3604249c2375058715e623b2cd22c61e176c
Siim <siim.teresk@veebiteenused.ee>
2020-11-26 01:20:05 +0200

    use random range instead

--- engine/engine.randomizer.py
+++ engine/engine.randomizer.py
@@ -14,11 +14,11 @@ class RandomizerEngine:
     def randomPause(self):
         # Minimum pause
         if time.time() - self.lastPause > 300:
-            randomData = random.random() * 100
+            randomData = random.randint(0, 100)
             # 3% chance to make a pause
             if randomData > 97:
                 # make a random pause. (up to 3 min)
                 self.lastPause = time.time()
-                pauseDuration = random.random() * 180
+                pauseDuration = random.randint(90, 180)
                 self.output.log("Starting a random pause for " + str(pauseDuration) + " seconds.")
                 time.sleep(pauseDuration)
