8166cdfebe7af1d103f22cbfdf108a17c4c8219e
Siim Teresk <siim.teresk@veebiteenused.ee>
2020-11-28 01:05:22 +0200

    Start capturing game windows

--- engine/engine.capture.py
+++ engine/engine.capture.py
@@ -1,25 +1,53 @@
 import pyautogui
-#import win32gui
+import win32gui
 import time
 import os
-from PIL import ImageGrab
+import numpy as np
+import PIL
 from pynput.mouse import Controller
 
 # Class CaptureEngine
 # Screenshots etc...
 
 class CaptureEngine:
+    activeWindows = {}
+
     def __init__(self, output):
         self.output = output
         self.basePath = os.path.dirname(os.path.realpath(__file__))
         self.output.log("Capture engine initialized.")
+        win32gui.EnumWindows(self.windowHandler, None)
+
+    def windowHandler(self, hwnd, ctx):
+        if win32gui.IsWindowVisible(hwnd):
+            if win32gui.GetWindowText(hwnd):
+                self.activeWindows[hwnd] = win32gui.GetWindowText(hwnd)
+
+    def getActiveWindowsTitles(self):
+        result = []
+        for handle in self.activeWindows.items():
+            result.append(handle[1])
+
+        return result
+
+    def getWindowFromHandle(self, handle):
+        for win in self.activeWindows.items():
+            if str(win[0]) in handle:
+                return win
+
+    def compareImages(self, img1, img2):
+        img_a_pixels = Image.open(img1).getdata()
+        img_b_pixels = Image.open(img2).getdata()
+        img_a_array = np.array(img_a_pixels)
+        img_b_array = np.array(img_b_pixels)
+
+        return (img_a_array == img_b_array).sum()
 
     def screenshot(self, window_title=None):
         if window_title:
             hwnd = win32gui.FindWindow(None, window_title)
             if hwnd:
-                # Set window to foreground
-                win32gui.SetForegroundWindow(hwnd)
+                self.output.log("[" + str(time.strftime("%H:%M:%S")) + "] Capturing window '" + window_title + "' screenshot.")
 
                 # get window positions
                 x, y, x1, y1 = win32gui.GetClientRect(hwnd)
@@ -28,29 +56,18 @@ class CaptureEngine:
 
                 # take screenshot
                 im = pyautogui.screenshot(region=(x, y, x1, y1))
-                return im
+                return self.formatImage(im)
             else:
                 raise ValueError("CaptureEngine, screen not found.")
         else:
             im = pyautogui.screenshot()
-            return im
+            return self.formatImage(im)
 
-    def screenshotRegion(self, dimensions):
-        # take screenshot
-        im = pyautogui.screenshot(region=dimensions)
-        im.save(self.basePath + '/../data/screenshot/screenshot.dimension.' + str(time.time()) + '.jpg')
-        return im
-
-    def screenshotMouse(self, size):
-        mouse = Controller()
-        x = mouse.position[0]
-        y = mouse.position[1]
-
-        # take screenshot
-        im = pyautogui.screenshot(region=(x, y, x + size[0], y + size[1]))
-        im.save(self.basePath + '/../data/screenshot/screenshot.mouse.' + str(time.time()) + '.jpg')
-        return im
+    def formatImage(self, imageObj):
+        maxsize = (1028, 1028)
+        imageObj.thumbnail(maxsize, PIL.Image.ANTIALIAS)
+        return imageObj
 
     def getPixel(self, x, y):
-        px = ImageGrab.grab().load()
+        px = PIL.ImageGrab.grab().load()
         return px[x, y]
--- scripts/script.ai.py
+++ scripts/script.ai.py
@@ -1,11 +1,15 @@
 import time
 import os
 import imp
+import win32gui
 from tkinter import *
+import _thread
 
 class Script:
     name = "AI"
     status = "stopped"
+    recordStatus = False
+    recordingThread = None
 
     def __init__(self, outputEngine):
         self.output = outputEngine
@@ -15,9 +19,69 @@ class Script:
 
     def process(self):
         self.statusLabel = self.createLabel(self.container, "AI Status")
+        self.windowSelection = self.createSelection(self.container, self.captureEngine.activeWindows.items())
+        self.actionType = self.createEntry(self.container, 'Action type')
+        self.recordBtn = self.createButton(self.container, "Record", self.startRecord)
         self.statusLabel.set("AI process started.")
+        self.recordingThread = _thread.start_new_thread(self.recordLoop, ())
 
-        self.captureEngine.screenshotMouse((100, 100))
+    def startRecord(self):
+        if self.recordStatus == False:
+            self.recordStatus = True
+            self.statusLabel.set("Recording gameplay for data.")
+            self.recordBtn.set('Recording')
+        else:
+            self.recordStatus = False
+            self.statusLabel.set("Recording stopped.")
+            self.recordBtn.set('Record')
+
+    def recordLoop(self):
+        self.output.log("Record loop started")
+        while True:
+            time.sleep(1)
+            if self.recordStatus == True:
+                activeHandle = win32gui.GetForegroundWindow()
+                selectedWindow = self.windowSelection.get()
+                selectedAction = self.actionType.get()
+                if len(selectedAction) > 0 and len(selectedWindow) > 0:
+                    if str(activeHandle) in selectedWindow:
+                        windowTitle = win32gui.GetWindowText(activeHandle)
+                        # Screenshot
+                        shot = self.captureEngine.screenshot(window_title = windowTitle)
+                        shot.save(self.basePath + '/../data/screenshot/' + windowTitle + '/screenshot.' + selectedAction + '.' + str(time.time()) + '.jpg')
+
+    def changeWindow(self, event):
+        selectedWindow = self.captureEngine.getWindowFromHandle(self.windowSelection.get())
+        # Make data storage
+        try:
+            os.makedirs(self.basePath + '/../data/screenshot/' + selectedWindow[1])
+        except OSError as e:
+            self.output.log("Path already exists for " + selectedWindow[1])
+
+    def halt(self):
+        self.status = "stopped"
+
+    def setContainer(self, container):
+        self.container = container
+
+    def createEntry(self, frame, label):
+        top = Frame(frame)
+        top.pack()
+        L1 = Label(top, text=label)
+        L1.pack( side = LEFT)
+        E1 = Entry(top, bd =5)
+        E1.pack(side = RIGHT)
+
+        return E1
+
+    def createSelection(self, frame, options):
+        variable = StringVar()
+
+        w = OptionMenu(frame, variable, *options, command = self.changeWindow)
+        w.config(width=20)
+        w.pack()
+
+        return variable
 
     def createLabel(self, frame, text):
         v = StringVar()
@@ -27,8 +91,10 @@ class Script:
 
         return v
 
-    def halt(self):
-        self.status = "stopped"
+    def createButton(self, frame, name, callback):
+        v = StringVar()
+        btn = Button(frame, textvariable=v, command = callback, padx = 60)
+        btn.pack()
+        v.set(name)
 
-    def setContainer(self, container):
-        self.container = container
+        return v
