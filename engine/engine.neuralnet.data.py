771ea1e7fae65f94b6b45f9bf64220c4be21d028
Siim Teresk <siim.teresk@veebiteenused.ee>
2020-11-28 03:23:36 +0200

    fix lib

--- engine/engine.neuralnet.data.py
+++ engine/engine.neuralnet.data.py
@@ -1,4 +1,5 @@
 import os
+from keras.preprocessing import image
 
 # Class NeuralNetDataEngine
 # Neural network data functionality.
@@ -7,11 +8,11 @@ class NeuralNetDataEngine:
     def __init__(self, output):
         basePath = os.path.dirname(os.path.realpath(__file__))
         self.output = output
-        self.output.log("Neural net initialized.")
+        self.output.log("Neural net data engine initialized.")
 
     def imageToData(self, path):
-        image = tf.keras.preprocessing.image.load_img(path)
-        input_arr = keras.preprocessing.image.img_to_array(image)
+        image = image.load_img(path)
+        input_arr = image.img_to_array(image)
         input_arr = np.array([input_arr])  # Convert single image to a batch.
         return input_arr
 
